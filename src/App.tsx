8bd5ea2fb821a8d2c27bb259667845a67b9f2b76
mallorysmith64 <mallorysmith64@gmail.com>
2025-11-24 07:36:21 -0500

    added cats, styling around cats, added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 import OctodexCats from './OctodexCats';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <OctodexCats />
       </header>
     </div>
--- src/OctodexCats.jsx
+++ src/OctodexCats.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import fintech_cat from "./images/fintech_cat.jpg";
 import boxertocat from "./images/boxertocat.jpg";
 import hula_loop_cat from "./images/hula_loop_cat.gif";
+import puddle_jumper_cat from "./images/puddle_jumper_cat.jpg";
+import scubatocat from "./images/scubatocat.png";
+import securitocat from "./images/securitocat.png";
+import surftocat from "./images/surftocat.png";
+import vinyltocat from "./images/vinyltocat.png";
+import dinotocat from "./images/dinotocat.png";
 
 function Header() {
   return (
@@ -9,22 +15,68 @@ function Header() {
     <section className="cats">
       <img
         src={fintech_cat}
-        width="200"
-        height="200"
+        width="300"
+        height="300"
+        className="cat"
         alt="fintech octodex cat"
       />
       <img
         src={boxertocat}
-        width="200"
-        height="200"
+        width="300"
+        height="300"
+        className="cat"
         alt="boxertocat octodex cat"
       />
       <img
         src={hula_loop_cat}
-        width="200"
-        height="200"
+        width="300"
+        height="300"
+        className="cat"
         alt="hula loop cat octodex cat"
       />
+      <img
+      src={puddle_jumper_cat}
+      width="300"
+      height="300"
+      className="cat"
+      alt="puddle jumper cat octodex cat"
+    />
+    <img
+    src={scubatocat}
+    width="300"
+    height="300"
+    className="cat"
+    alt="scubatocat octodex cat"
+  />
+  <img 
+  src={securitocat}
+  width="300"
+  height="300"
+  className="cat"
+  alt="securitocat octodex cat"
+
+/>
+<img
+src={vinyltocat}
+width="300"
+height="300"
+className="cat"
+alt="vinyltocat octodex cat"
+/>
+<img
+src={surftocat}
+width="300"
+height="300"
+className="cat"
+alt="surftocat octodex cat"
+/>
+<img
+src={dinotocat}
+width="300"
+height="300"
+className="cat"
+alt="dintocat octodex cat"
+/>
       </section>
     </>
   );
